a25c8f020bab61d944a775afd9a2eaf15f0771d9
しょうご <155814857+Shogo-Kai@users.noreply.github.com>
2024-05-19 20:37:34 +0900

    ルートの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Router } from './routes/Router';
+import { Home } from './pages/Home';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Router />
+      <Home />
     </div>
   );
 }
